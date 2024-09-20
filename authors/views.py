647a4766e81c57c36128ef6aca564ac256918111
Abbos Zoirov <abboszoirov17@gmail.com>
2024-09-20 19:00:22 +0500

    add views to authors app

--- authors/views.py
+++ authors/views.py
@@ -1,3 +1,77 @@
-from django.shortcuts import render
+from .models import Author
+from .serializers import AuthorSerializer
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK, HTTP_204_NO_CONTENT, HTTP_201_CREATED
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.parsers import MultiPartParser, FormParser
+
+from drf_spectacular.utils import extend_schema
+
+
+class AuthorListAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+    parser_classes = MultiPartParser, FormParser
+
+    @extend_schema(
+        request=
+            { 
+            'application/json': AuthorSerializer, 
+            'multipart/form-data': AuthorSerializer  
+            },
+        responses=AuthorSerializer
+    )
+    def post(self, request):
+        serializer = AuthorSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(data=serializer.data, status=HTTP_201_CREATED)
+    
+    @extend_schema(
+        responses=AuthorSerializer
+    )
+    def get(self, request):
+        authors = Author.objects.all()
+        serializer = AuthorSerializer(authors, many=True)
+
+        return Response(data=serializer.data, status=HTTP_200_OK)
+    
+
+class AuthorDetailAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+    parser_classes = [MultiPartParser, FormParser]
+
+    @extend_schema(
+        request=
+            { 
+            'application/json': AuthorSerializer, 
+            'multipart/form-data': AuthorSerializer  
+            },
+        responses=AuthorSerializer
+    )
+    def put(self, request, pk):
+        author = Author.objects.get(pk=pk)
+        serializer = AuthorSerializer(author, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(data=serializer.data, status=HTTP_200_OK)
+    
+    @extend_schema(
+        responses=AuthorSerializer
+    )
+    def get(self, request, pk):
+        author = Author.objects.get(pk=pk)
+        serializer = AuthorSerializer(author)
+        return Response(data=serializer.data, status=HTTP_200_OK)
+    
+    
+    @extend_schema(
+        responses=AuthorSerializer
+    )
+    def delete(self, request, pk):
+        author = Author.objects.get(pk=pk)
+        author.delete()
+        return Response(status=HTTP_204_NO_CONTENT)
