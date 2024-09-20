f3a86726df92d7781d7c647f6cc9fbad73d1c784
Abbos Zoirov <abboszoirov17@gmail.com>
2024-09-20 15:19:55 +0500

    migrations

--- core/settings.py
+++ core/settings.py
@@ -104,10 +104,15 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bibliopedia_db',  
+        'USER': 'postgres',   
+        'PASSWORD': '1234',    
+        'HOST': 'localhost',            
+        'PORT': '5433',                 
     }
 }
+
 
 
 # Password validation
