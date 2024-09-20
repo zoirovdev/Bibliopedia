1e5631d712e13e9c1abef9569b2fefb311226ff1
Abbos Zoirov <abboszoirov17@gmail.com>
2024-09-20 14:53:00 +0500

    update users/urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    
+    path('all/', views.UserList.as_view()),
+    path('user/', views.UserAPIView.as_view()),
+    path('register/', views.RegisterAPIView.as_view()),
+    path('login/', views.LoginAPIView.as_view()),
+    path('logout/', views.LogoutAPIView.as_view()),
+    path('make-admin/<str:email>', views.MakeAdminAPIView.as_view())
 ]
