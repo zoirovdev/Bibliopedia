1957838a83e933a0c4e0dc4acdf7967a70805324
Abbos Zoirov <abboszoirov17@gmail.com>
2024-09-20 20:42:57 +0500

    add QuoteAPIView

--- quotes/views.py
+++ quotes/views.py
@@ -1,3 +1,61 @@
-from django.shortcuts import render
+from .models import Quote
+from .serializers import QuoteSerializer
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_204_NO_CONTENT
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+
+from drf_spectacular.utils import extend_schema
+
+
+class QuoteAPIView(APIView):
+    permission_classes = [IsAdminUser, IsAuthenticated]
+
+    @extend_schema(
+        request=
+            { 
+            'application/json': QuoteSerializer, 
+            'multipart/form-data': QuoteSerializer  
+            },
+        responses=QuoteSerializer
+    )
+    def post(self, request):
+        serializer = QuoteSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(data=serializer.data, status=HTTP_201_CREATED)
+    
+    @extend_schema(
+        request=
+            { 
+            'application/json': QuoteSerializer, 
+            'multipart/form-data': QuoteSerializer  
+            },
+        responses=QuoteSerializer
+    )
+    def put(self, request, pk):
+        quote = Quote.objects.get(pk=pk)
+        serializer = QuoteSerializer(quote, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(data=serializer.data, status=HTTP_200_OK)
+    
+    @extend_schema(
+        responses=QuoteSerializer
+    )
+    def delete(self, request, pk):
+        quote = Quote.objects.get(pk=pk)
+        quote.delete()
+        return Response(status=HTTP_204_NO_CONTENT)
+    
+    @extend_schema(
+        responses=QuoteSerializer
+    )
+    def get(self, request):
+        quotes = Quote.objects.order_by('?').all()
+        serializer = QuoteSerializer(quotes, many=True)
+        return Response(data=serializer.data, status=HTTP_200_OK)
+
